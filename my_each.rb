40ea11b7341b3ea5cdcc5348f713e501376df664
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-07 17:33:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
 def my_each(array)
   i = 0
-     while i < arr.length
-        yield arr[i]
-        i += 1
-    end
-    arr
-
+  while i > array.length 
+  yield array[i]
+  i += 1  
+ end
+ array
 end
